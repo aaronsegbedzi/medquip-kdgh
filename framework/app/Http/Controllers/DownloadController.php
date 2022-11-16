0fa555165fa7484cd0fa83e120edfeaa16723bb8
aaronsegbedzi <aaronsegbedzi@outlook.com>
2022-11-16 11:14:50 +0000

    Update DownloadController.php

--- framework/app/Http/Controllers/DownloadController.php
+++ framework/app/Http/Controllers/DownloadController.php
@@ -7,7 +7,10 @@ use Illuminate\Http\Request;
 class DownloadController extends Controller
 {
     public function mobileApp() {
+        $headers = array(
+            'Content-Type: application/vnd.android.package-archive',
+        );
         $file_path = public_path('kdghmobile.apk');
-        return response()->download($file_path);
+        return response()->download($file_path,null,$headers);
     }
 }
